81a45e0a414770ce9fb71e0e22c58feee895aca9
jinhao <jinhao_@hotmail.com>
2016-08-23 20:32:42 +0800

    updated the format and comments of the code to fit PEP-8 style

--- attackDetection.py
+++ attackDetection.py
@@ -4,28 +4,35 @@ import ConfigParser
 config = ConfigParser.ConfigParser()
 config.read("config.txt")
 
-activityOutputPath = config.get('paths','activityOutputPath')
-pathToSQLiOutput = config.get('parameters','SQLiOutput')
-pathToRemoteFileInclusionOutput = config.get('parameters','RemoteFileInclusionOutput')
-pathToWebShellsOutput = config.get('parameters','WebShellsOutput')
+activityOutputPath = config.get('paths', 'activityOutputPath')
+pathToSQLiOutput = config.get('parameters', 'SQLiOutput')
+pathToRemoteOutput = config.get('parameters', 'RFIOutput')
+pathToWebShellsOutput = config.get('parameters', 'WebShellsOutput')
 
-shells = ["r57","loaderz","c0derz","c99","kadot","h4ntu"]
-
-for file in os.listdir(activityOutputPath):
-	print "processing file "+file
-	with open(activityOutputPath+file) as f:
-		f = f.readlines()
-
-	for line in f:
-		if "union" in line:
-			with open(pathToSQLiOutput,"a+") as f:
-				f.write(file +":"+line)
-		if "/etc/passwd" in line:
-			with open(pathToRemoteFileInclusionOutput,"a+") as f:
-				f.write(file +":"+line)
-		for item in shells:
-			if item in line:
-				with open(pathToWebShellsOutput,"a+") as f:
-					f.write(file +":"+line)
+shells = ["r57", "loaderz", "c0derz", "c99", "kadot", "h4ntu"]
 
 
+def attackDetection():
+	""" Attempts to detect web based attack through the log files
+	SQLi attacks are found through matching of the word "UNION"
+	Remote file inclusions are found through matching "/etc/passwd"
+	Web shells are found through matching the predefine list of common shells
+	"""
+    for file in os.listdir(activityOutputPath):
+        print "processing file " + file
+        with open(activityOutputPath + file) as f:
+            f = f.readlines()
+
+        for line in f:
+            if "union" in line:
+                with open(pathToSQLiOutput, "a+") as f:
+                    f.write(file + ":" + line)
+            if "/etc/passwd" in line:
+                with open(pathToRemoteFileInclusionOutput, "a+") as f:
+                    f.write(file + ":" + line)
+            for item in shells:
+                if item in line:
+                    with open(pathToWebShellsOutput, "a+") as f:
+                        f.write(file + ":" + line)
+
+attackDetection()
--- logAnalyzer.py
+++ logAnalyzer.py
@@ -4,99 +4,99 @@ import re
 from geoip import geolite2
 
 config = ConfigParser.ConfigParser()
-
 config.read("config.txt")
 
+
 def getIpInfo(inputFile):
+	""" Reads in a flat input file
 
-	infile = inputFile
-	
-	ipAddressHits = {}
-	ip2Country = {}
-	ip2Activity = {}
+	Returns 3 dictionaries:
 
-	count = 0
+	1) ipAddressCount
+	   Key: IP Address
+	   Finds the number of times an IP address appears
 
-	with open(infile) as f:
-    		f = f.readlines()
-	
-	print "reading in file"
-	for line in f:
-		try:
+	2) ip2Country
+       Key: IP Address
+	   Find the country of origin for the IP addresses
 
-			# using regex to extract out current ip address in the line
-			curIpAdd = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
-	
-			# regex returns 2 ips, interested in the second
-			targetIp = curIpAdd[1]
+	3) ip2Activity
+       Key: IP Address
+	   Associates all activity in the logfile to the IP address
+	"""
 
-			# 1) only adding in unique ip addreses, set initial hit to 1
-			# 2) if ip is already in the list, increment the hits by 1
-			# 3) look up the country for the ip
-			# 4) initializing a dynamic dictionary for activity
-			if (targetIp not in ipAddressHits):
-				ipAddressHits[targetIp] = 1
-			
-				match = geolite2.lookup(targetIp)
-				if match is not None and match.country is not None:
-					ip2Country[targetIp] = match.country
-				else:
-					ip2Country[targetIp] = "not found"
-			
-				ip2Activity.setdefault(targetIp,[])
-							
-			else:
-				ipAddressHits[targetIp] += 1
-			
-			#adding each line of activity to the respective IP
-			ip2Activity[targetIp].append(line)
-		
-		except Exception,e:
-			continue
+    ipAddressCount = {}
+    ip2Country = {}
+    ip2Activity = {}
 
+    with open(infile) as f:
+	    for line in f:
+	        try:
 
+	            # using regex to extract out current ip address in the line
+	            curIpAdd = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
 
-	return ipAddressHits, ip2Country, ip2Activity
+	            # regex returns 2 ips, interested in the second IP
+	            targetIp = curIpAdd[1]
 
-	
+	            # 1) only adding in unique ip addreses, set initial count to 1
+	            # 2) if ip is already in the list, increment the count by 1
+	            # 3) look up the country for the ip
+	            # 4) initializing a dynamic dictionary for each ip activity
+	            if (targetIp not in ipAddressCount):
+	                ipAddressCount[targetIp] = 1
 
-def writeToFile(filename,content):
-	with open(filename,"a+") as f:
-		f.write(content)
+	                match = geolite2.lookup(targetIp)
+	                if match is not None and match.country is not None:
+	                    ip2Country[targetIp] = match.country
+	                else:
+	                    ip2Country[targetIp] = "not found"
+	                ip2Activity.setdefault(targetIp, [])
+	            else:
+	                ipAddressCount[targetIp] += 1
+
+	            # adding each line of activity to the respective IP
+	            ip2Activity[targetIp].append(line)
+	        except Exception, e:
+	            continue
+
+    return ipAddressCount, ip2Country, ip2Activity
 
 
+def writeToFile(filename, content):
+	""" Appends the given content to a file"""
+    with open(filename, "a+") as f:
+        f.write(content)
 
-	
-inputPath = config.get('paths','inputPath')
-outputPath = config.get('paths','outputPath')
-activityOutputPath = config.get('paths','activityOutputPath')
-filename = config.get('parameters','filename')
 
-dirs = [inputPath,outputPath,activityOutputPath]
+inputPath = config.get('paths', 'inputPath')
+outputPath = config.get('paths', 'outputPath')
+activityOutputPath = config.get('paths', 'activityOutputPath')
+filename = config.get('parameters', 'filename')
+
+dirs = [inputPath, outputPath, activityOutputPath]
 
 for directory in dirs:
-	if not os.path.exists(directory):
-    		os.makedirs(directory)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 unqiueIpName = "unqiueIp.txt"
 ipCountryHitsName = "ipCountryHits.txt"
-
 inputFile = inputPath + filename
 
-[ipAddressHits,ip2Country,ip2Activity] = getIpInfo(inputFile)
+ipAddressCount, ip2Country, ip2Activity] = getIpInfo(inputFile)
 
 
 print "writing unique addresses"
-for ip in ipAddressHits:
-	writeToFile(outputPath+unqiueIpName,ip+"\n")
+for ip in ipAddressCount:
+    writeToFile(outputPath + unqiueIpName, ip + "\n")
 
 print "writing unique addresses, country and hits"
-for ip in ipAddressHits:
-	line = ip+"|"+str(ipAddressHits[ip])+"|"+ip2Country[ip]+"\n"
-	writeToFile(outputPath+ipCountryHitsName,line)
+for ip in ipAddressCount:
+    line = ip + "|" + str ipAddressCount[ip]) + "|" + ip2Country[ip] + "\n"
+    writeToFile(outputPath + ipCountryHitsName, line)
 
 print "writing activity"
 for ip in ip2Activity:
-	for item in ip2Activity[ip]:
-		writeToFile(activityOutputPath+ip,item)
-
+    for item in ip2Activity[ip]:
+        writeToFile(activityOutputPath + ip, item)
